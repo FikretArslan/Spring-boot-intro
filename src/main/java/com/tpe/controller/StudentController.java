435178c6ec42719e3a6417be69b4a2413bf1d177
FikretArslan <arslanfikret33@gmail.com>
2022-06-20 17:25:26 +0300

    20/06/2022

---
+++ src/main/java/com/tpe/controller/StudentController.java
@@ -0,0 +1,89 @@
+package com.tpe.controller;
+
+import com.tpe.domain.Student;
+import com.tpe.dto.StudentDTO;
+import com.tpe.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+	
+	@Autowired
+	private StudentService studentService;
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to Student Controller";	
+	}
+	
+	@PostMapping
+	public ResponseEntity<Map<String,String>> createStudent(@Valid @RequestBody Student student){
+		studentService.createStudent(student);
+		Map<String,String> map=new HashMap<>();
+		map.put("message", "Student created successfuly");
+		map.put("status", "true");
+		return new ResponseEntity<>(map, HttpStatus.CREATED);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Student>> getAll(){
+		List<Student> students = studentService.getAll();
+		return ResponseEntity.ok(students);
+	} 
+	
+	
+	@GetMapping("/query")
+	public ResponseEntity<Student> getStudent(@RequestParam("id") Long id){
+		Student student= studentService.findStudent(id);
+		return ResponseEntity.ok(student);
+	} 
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Student> getStudentWithPath(@PathVariable("id") Long id){
+		Student student= studentService.findStudent(id);
+		return ResponseEntity.ok(student);
+	} 
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Map<String,String>> deleteStudent(@PathVariable("id") Long id){
+		studentService.deleteStudent(id);
+		Map<String,String> map=new HashMap<>();
+		map.put("message", "Student deleted successfuly");
+		map.put("status", "true");
+		return new ResponseEntity<>(map,HttpStatus.OK);
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity<Map<String,String>> updateStudent(@PathVariable Long id,@Valid @RequestBody StudentDTO studentDTO){
+		studentService.updateStudent(id,studentDTO);
+		Map<String,String> map=new HashMap<>();
+		map.put("message", "Student updated successfuly");
+		map.put("status", "true");
+		return new ResponseEntity<>(map,HttpStatus.OK);
+	}
+	
+	
+	@GetMapping("/pages")
+	public ResponseEntity<Page<Student>> getAllWithPage(@RequestParam("page") int page, @RequestParam("size") int size,
+														@RequestParam("sort") String prop, @RequestParam("direction") Sort.Direction direction
+			){
+		Pageable pageable= PageRequest.of(page, size, Sort.by(direction,prop));
+		Page<Student> studentPage= studentService.getAllWithPage(pageable);
+		return ResponseEntity.ok(studentPage);
+	}
+	
+	
+}
---
+++ src/main/java/com/tpe/repository/StudentRepository.java
@@ -0,0 +1,23 @@
+package com.tpe.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tpe.domain.Student;
+import com.tpe.exception.ConflictException;
+
+@Repository
+
+//Repository interface
+//CRUDRepository-CRUD Operasyonları
+//JPARepository-->CRUD + PagingAndSortingRepository
+//PagingAndSortingRepository-->Paging ve Sorting
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+	List<Student> findByLastName(String lastName);
+	
+	Boolean existsByEmail(String email) throws ConflictException;
+	
+}
